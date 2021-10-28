8406fd10e6c775b8b02070f8ac8c1f3a58a5cbc9
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-10-28 18:28:32 +0100

    :zap: Project cleanup and Chakra UI setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component }) {
+  return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
