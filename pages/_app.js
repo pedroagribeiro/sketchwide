cc9a1b6906ced57d75f88eebd6fecfeaea359056
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-11-04 14:19:46 +0000

    :art: Updates project font to Inter

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import Theme from "../theme/Fonts";
 
 function App({ Component }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={Theme}>
       <Component />
     </ChakraProvider>
   );
